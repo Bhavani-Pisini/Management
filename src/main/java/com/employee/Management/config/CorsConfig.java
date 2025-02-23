8475a9d38abb739f9728b746f406533e7566085d
Bhavani-Pisini <pisinibhavani03@gmail.com>
2025-02-23 20:01:58 +0530

    frontend deployed url added

--- src/main/java/com/employee/Management/config/CorsConfig.java
+++ src/main/java/com/employee/Management/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173","https://employee-management-ui-iota.vercel.app/employees")
+                        .allowedOrigins("http://localhost:5173","https://employee-management-ui-iota.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
